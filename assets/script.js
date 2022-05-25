e96616d2c32b37439904aa173879c2e1fa013c10
Jeffry Langford <jblangford97@gmail.com>
2022-05-25 18:06:43 -0500

    finished things

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,140 @@
 // creating time on the page
 var dateTimeEl = document.getElementById("currentDay");
 var today = new Date();
-var hour = today.getHours();
-var time = hour + ":" + today.getMinutes();
-dateTimeEl.innerHTML = today.toLocaleDateString('en-US', { timeZone: 'America/Chicago' }) + " " + time;
+dateTimeEl.innerHTML = today.toLocaleDateString('en-US', {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true });
+
+// grabbing buttons
+var saveBtnEl = $(".btn-primary");
+var clearBtnEl = $(".btn-danger");
 
 // grabbing each input
-var nineEL = document.getElementById("nine");
-var tenEL = document.getElementById("ten");
-var elevenEL = document.getElementById("eleven");
-var twelveEL = document.getElementById("twelve");
-var oneEL = document.getElementById("one");
-var twoEL = document.getElementById("two");
-var threeEL = document.getElementById("three");
-var fourEL = document.getElementById("four");
-var fiveEL = document.getElementById("five");
+var nineEl = document.getElementById("nine");
+var tenEl = document.getElementById("ten");
+var elevenEl = document.getElementById("eleven");
+var twelveEl = document.getElementById("twelve");
+var oneEl = document.getElementById("one");
+var twoEl = document.getElementById("two");
+var threeEl = document.getElementById("three");
+var fourEl = document.getElementById("four");
+var fiveEl = document.getElementById("five");
 
+// adding color depending on past, present, future
+function changeColor () {
+    let hour = $("input").text().trim();
+
+    let time = moment(hour, "LT");
+
+    $("input").removeClass("bg-success bg-secondary bg-danger")
+
+    if (moment().isAfter(time)) {
+        $("input").addClass("bg-secondary");
+    } else if (moment().isBefore(time)) {
+        $("input").addClass("bg-success");
+    } else {
+        $("input").addClass("bg-danger");
+    }
+};
+
+// if (hour < 9) {
+//     nineEl.classList.add("bg-success");
+// } else if (hour = 9) {
+//     nineEl.classList.add("bg-danger");
+// } else {
+//     nineEl.classList.add("bg-secondary");
+// };
+
+// if (hour < 10) {
+//     tenEl.classList.add("bg-success");
+// } else if (hour = 10) {
+//     tenEl.classList.add("bg-danger");
+// } else {
+//     tenEl.classList.add("bg-secondary");
+// };
+
+// if (hour < 11) {
+//     elevenEl.classList.add("bg-success");
+// } else if (hour = 11) {
+//     elevenEl.classList.add("bg-danger");
+// } else {
+//     elevenEl.classList.add("bg-secondary");
+// };
+
+// if (hour < 12) {
+//     twelveEl.classList.add("bg-success");
+// } else if (hour = 12) {
+//     twelveEl.classList.add("bg-danger");
+// } else {
+//     twelveEl.classList.add("bg-secondary");
+// };
+
+// if (hour < 13) {
+//     oneEl.classList.add("bg-success");
+// } else if (hour = 13) {
+//     oneEl.classList.add("bg-danger");
+// } else {
+//     oneEl.classList.add("bg-secondary");
+// };
+
+// if (hour < 14) {
+//     twoEl.classList.add("bg-success");
+// } else if (hour = 14) {
+//     twoEl.classList.add("bg-danger");
+// } else {
+//     twoEl.classList.add("bg-secondary");
+// };
+
+// if (hour < 15) {
+//     threeEl.classList.add("bg-success");
+// } else if (hour = 15) {
+//     threeEl.classList.add("bg-danger");
+// } else {
+//     threeEl.classList.add("bg-secondary");
+// };
+
+// if (hour < 16) {
+//     fourEl.classList.add("bg-success");
+// } else if (hour = 16) {
+//     fourEl.classList.add("bg-danger");
+// } else {
+//     fourEl.classList.add("bg-secondary");
+// };
+
+// if (hour < 17) {
+//     five.classList.add("bg-success");
+// } else if (hour = 17) {
+//     five.classList.add("bg-danger");
+// } else {
+//     five.classList.add("bg-secondary");
+// };
+
+// button functionality
+function saveClickHandler() {
+    var userText = $(this).siblings("input").val()
+    var timeInput = $(this).siblings("input").attr("id")
+    localStorage.setItem(timeInput, userText)
+}
+
+function clearClickHandler() {
+    localStorage.clear();
+    location.reload();
+}
+
+// local storage functionality
+$("#nine").val(localStorage.getItem("nine"));
+$("#ten").val(localStorage.getItem("ten"));
+$("#eleven").val(localStorage.getItem("eleven"));
+$("#twelve").val(localStorage.getItem("twelve"));
+$("#one").val(localStorage.getItem("one"));
+$("#two").val(localStorage.getItem("two"));
+$("#three").val(localStorage.getItem("three"));
+$("#four").val(localStorage.getItem("four"));
+$("#five").val(localStorage.getItem("five"));
+
+// event listener through JQuery
+saveBtnEl.on("click", saveClickHandler);
+clearBtnEl.on("click", clearClickHandler);
+
+changeColor();
