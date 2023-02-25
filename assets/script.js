147248972d0b076e5030bf0d12603067e8efe5b9
Jeffry Langford <jblangford97@gmail.com>
2023-02-24 20:52:57 -0600

    added additional hours, cleaned up a bit

--- assets/script.js
+++ assets/script.js
@@ -11,6 +11,8 @@ var saveBtnEl = $(".btn-primary");
 var clearBtnEl = $(".btn-danger");
 
 // grabbing each input
+var sevenEl = document.getElementById("seven");
+var eightEl = document.getElementById("eight");
 var nineEl = document.getElementById("nine");
 var tenEl = document.getElementById("ten");
 var elevenEl = document.getElementById("eleven");
@@ -20,109 +22,46 @@ var twoEl = document.getElementById("two");
 var threeEl = document.getElementById("three");
 var fourEl = document.getElementById("four");
 var fiveEl = document.getElementById("five");
+var sixEl = document.getElementById("six");
 
 // adding color depending on past, present, future
 function changeColor () {
-    let hour = $("input").text().trim();
-
-    let time = moment(hour, "LT");
 
     $("input").removeClass("bg-success bg-secondary bg-danger")
 
-    if (moment().isAfter(time)) {
-        $("input").addClass("bg-secondary");
-    } else if (moment().isBefore(time)) {
-        $("input").addClass("bg-success");
-    } else {
-        $("input").addClass("bg-danger");
-    }
+    $(".row .col-1:first-child").each(function(index) {
+        let hour = $(this).text()
+        let time = moment(hour, "LT")
+        
+        if (moment().isAfter(time)) {
+            $(this).next().addClass("bg-secondary")
+        } else {
+            $(this).next().addClass("bg-success")
+        }
+        
+        if (moment().hour() == time.hour()) {
+            $(this).next().addClass("bg-danger")
+        }
+    
+    })
+
 };
-
-// if (hour < 9) {
-//     nineEl.classList.add("bg-success");
-// } else if (hour = 9) {
-//     nineEl.classList.add("bg-danger");
-// } else {
-//     nineEl.classList.add("bg-secondary");
-// };
-
-// if (hour < 10) {
-//     tenEl.classList.add("bg-success");
-// } else if (hour = 10) {
-//     tenEl.classList.add("bg-danger");
-// } else {
-//     tenEl.classList.add("bg-secondary");
-// };
-
-// if (hour < 11) {
-//     elevenEl.classList.add("bg-success");
-// } else if (hour = 11) {
-//     elevenEl.classList.add("bg-danger");
-// } else {
-//     elevenEl.classList.add("bg-secondary");
-// };
-
-// if (hour < 12) {
-//     twelveEl.classList.add("bg-success");
-// } else if (hour = 12) {
-//     twelveEl.classList.add("bg-danger");
-// } else {
-//     twelveEl.classList.add("bg-secondary");
-// };
-
-// if (hour < 13) {
-//     oneEl.classList.add("bg-success");
-// } else if (hour = 13) {
-//     oneEl.classList.add("bg-danger");
-// } else {
-//     oneEl.classList.add("bg-secondary");
-// };
-
-// if (hour < 14) {
-//     twoEl.classList.add("bg-success");
-// } else if (hour = 14) {
-//     twoEl.classList.add("bg-danger");
-// } else {
-//     twoEl.classList.add("bg-secondary");
-// };
-
-// if (hour < 15) {
-//     threeEl.classList.add("bg-success");
-// } else if (hour = 15) {
-//     threeEl.classList.add("bg-danger");
-// } else {
-//     threeEl.classList.add("bg-secondary");
-// };
-
-// if (hour < 16) {
-//     fourEl.classList.add("bg-success");
-// } else if (hour = 16) {
-//     fourEl.classList.add("bg-danger");
-// } else {
-//     fourEl.classList.add("bg-secondary");
-// };
-
-// if (hour < 17) {
-//     five.classList.add("bg-success");
-// } else if (hour = 17) {
-//     five.classList.add("bg-danger");
-// } else {
-//     five.classList.add("bg-secondary");
-// };
 
 // button functionality
 function saveClickHandler() {
     var userText = $(this).siblings("input").val()
     var timeInput = $(this).siblings("input").attr("id")
     localStorage.setItem(timeInput, userText)
-}
+};
 
 function clearClickHandler() {
     localStorage.clear();
     location.reload();
-}
+};
 
 // local storage functionality
+$("#seven").val(localStorage.getItem("seven"));
+$("#eight").val(localStorage.getItem("eight"));
 $("#nine").val(localStorage.getItem("nine"));
 $("#ten").val(localStorage.getItem("ten"));
 $("#eleven").val(localStorage.getItem("eleven"));
@@ -132,6 +71,7 @@ $("#two").val(localStorage.getItem("two"));
 $("#three").val(localStorage.getItem("three"));
 $("#four").val(localStorage.getItem("four"));
 $("#five").val(localStorage.getItem("five"));
+$("#six").val(localStorage.getItem("six"));
 
 // event listener through JQuery
 saveBtnEl.on("click", saveClickHandler);
